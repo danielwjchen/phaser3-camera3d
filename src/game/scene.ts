b6a97f6fc7a4b6a88c0eaa9ea485b9cc04b31e7a
Daniel Chen <s1300045@gmail.com>
2021-01-01 15:19:37 -0600

    Fixes typescript syntac warnings.

--- src/game/scene.ts
+++ src/game/scene.ts
@@ -5,12 +5,7 @@ import { Character, Command, Direction } from './character';
 export class MainScene extends Phaser.Scene {
 
     private spriteName: string = 'template';
-    private character: Character;
-    private leftKey: Phaser.Input.Keyboard.Key;
-    private rightKey: Phaser.Input.Keyboard.Key;
-    private upKey: Phaser.Input.Keyboard.Key;
-    private downKey: Phaser.Input.Keyboard.Key;
-    private runKey: Phaser.Input.Keyboard.Key;
+    private character: Character | undefined;
 
     constructor() {
         super({
@@ -30,58 +25,65 @@ export class MainScene extends Phaser.Scene {
     }
 
     create(): void {
-        this.character = new Character(
+        let character: Character = new Character(
             this, 
             this.cameras.main.centerX, 
             this.cameras.main.centerY, 
             this.spriteName
         );
-        this.leftKey = this.input.keyboard.addKey('LEFT');
-        this.rightKey = this.input.keyboard.addKey('RIGHT');
-        this.upKey = this.input.keyboard.addKey('UP');
-        this.downKey = this.input.keyboard.addKey('DOWN');
-        this.runKey = this.input.keyboard.addKey('r');
+        let leftKey: Phaser.Input.Keyboard.Key = 
+            this.input.keyboard.addKey('LEFT');
+        let rightKey: Phaser.Input.Keyboard.Key = 
+            this.input.keyboard.addKey('RIGHT');
+        let upKey: Phaser.Input.Keyboard.Key = 
+            this.input.keyboard.addKey('UP');
+        let downKey: Phaser.Input.Keyboard.Key = 
+            this.input.keyboard.addKey('DOWN');
+        let runKey: Phaser.Input.Keyboard.Key = 
+            this.input.keyboard.addKey('r');
         [
-            this.leftKey,
-            this.rightKey,
-            this.upKey,
-            this.downKey,
-            this.runKey,
+            leftKey,
+            rightKey,
+            upKey,
+            downKey,
+            runKey,
         ].forEach(key => {
             ['up', 'down'].forEach(eventName => {
                 key.on(eventName, () => {
                     let command: Command = {
-                        Up: this.upKey.isDown,
-                        Down: this.downKey.isDown,
-                        Left: this.leftKey.isDown,
-                        Right: this.rightKey.isDown,
+                        Up: upKey.isDown,
+                        Down: downKey.isDown,
+                        Left: leftKey.isDown,
+                        Right: rightKey.isDown,
                     };
-                    this.character.go(command, this.runKey.isDown);
-
+                    character.go(command, runKey.isDown);
                 });
             });
         });
         this.input.keyboard.addKey('b').on('down', () => {
-            this.character.attacked(100, Direction.Left);
+            character.attacked(100, Direction.Left);
         });
         this.input.keyboard.addKey('v').on('down', () => {
-            this.character.attacked(100, Direction.Right);
+            character.attacked(100, Direction.Right);
         });
         this.input.keyboard.addKey('a').on('down', () => {
-            this.character.attack();
+            character.attack();
         });
         this.input.keyboard.addKey('g').on('down', () => {
-            this.character.guard();
+            character.guard();
         });
         this.input.keyboard.addKey('g').on('up', () => {
-            this.character.stand();
+            character.stand();
         });
         this.input.keyboard.addKey('SPACE').on('down', () => {
-            this.character.jump();
+            character.jump();
         });
+
+        this.character = character;
     }
 
     update() {
+        if (!this.character) { return; }
         this.character.update();
     }
 }
