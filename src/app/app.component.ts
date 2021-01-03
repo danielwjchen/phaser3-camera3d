33ba502118fa3c09214dc7bb48fe0aec5d56f229
Daniel W. Chen <hello@danielwjchen.com>
2021-01-03 07:35:55 -0600

    Replaces import with plugin.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import * as Phaser from 'phaser';
 import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { MainScene } from 'src/game/scene';
 
+import {
+  PLUGIN_PLATFORMER, PlatformerPlugin
+} from 'src/plugins/platformer';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,18 +23,27 @@ export class AppComponent implements AfterViewInit, OnDestroy {
       return;
     }
     let gameConfig: Phaser.Types.Core.GameConfig = {
-        parent: this.canvasContainer.nativeElement,
-        type: Phaser.AUTO,
-        width: 800,
-        height: 600,
-        backgroundColor: '#18216D',
-        scene: new MainScene(),
-        physics: {
-          default: 'arcade',
-          arcade: {
-              gravity: { y: 200 }
-          }
+      parent: this.canvasContainer.nativeElement,
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      backgroundColor: '#18216D',
+      scene: new MainScene(),
+      physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
         }
+      },
+      plugins: {
+          scene: [
+            { 
+              key: PLUGIN_PLATFORMER, 
+              plugin: PlatformerPlugin, 
+              mapping: 'platformer',
+            }
+          ]
+      },
     };
 
     this.game = new Phaser.Game(gameConfig);
