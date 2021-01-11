10b7f68a25cf37cd22b642f5af08cb2421e37c3f
Daniel Chen <s1300045@gmail.com>
2021-01-10 18:05:06 -0600

    Adds polygon platform.

--- src/plugins/platformer/platform.ts
+++ src/plugins/platformer/platform.ts
@@ -1,19 +1,50 @@
 import { RADIAN_45, SQRT_2 } from './constants';
 
+const COLOR_GRID: number = 0x00aa00;
+
 export class Platform {
 
     constructor(
         public scene: Phaser.Scene, public width: number, public length: number
     ) {
-        let gridColor: number = 0x0B610B;
         let yHorizon: number = this.width / 2;
         let yOffsetForUi: number = this.width / 5;
 
         let yStage: number = this.width - yOffsetForUi;
         let gridSize: number = (yHorizon - yOffsetForUi) / 5;
-        this.createLinesX(gridSize, gridColor, yStage, yHorizon);
-        this.createLinesZ(gridSize, gridColor, yStage, yHorizon);
+        this.createPlane();
+        // this.createLinesX(gridSize, COLOR_GRID, yStage, yHorizon);
+        // this.createLinesZ(gridSize, COLOR_GRID, yStage, yHorizon);
 
+    }
+
+    private createPlane() {
+        let offset: number = (SQRT_2 / 2) * this.width;
+        let startingCanvasX: number = this.scene.game.canvas.width / 2 - offset;
+        let startingCanvasY: number = this.scene.game.canvas.height / 2 + this.width / 2;
+        let polygon: Phaser.Geom.Polygon = new Phaser.Geom.Polygon([
+            startingCanvasX, startingCanvasY,
+            startingCanvasX + offset, startingCanvasY - offset,
+            startingCanvasX + this.length + offset, startingCanvasY - offset,
+            startingCanvasX + this.length, startingCanvasY,
+            startingCanvasX, startingCanvasY,
+        ]);
+
+        let graphics = this.scene.add.graphics({ x: 0, y: 0 });
+
+        graphics.lineStyle(2, COLOR_GRID);
+
+        graphics.beginPath();
+
+        graphics.moveTo(polygon.points[0].x, polygon.points[0].y);
+
+        for (var i = 1; i < polygon.points.length; i++)
+        {
+            graphics.lineTo(polygon.points[i].x, polygon.points[i].y);
+        }
+
+        graphics.closePath();
+        graphics.strokePath();
     }
 
 
