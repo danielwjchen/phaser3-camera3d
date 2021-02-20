a92f40a68096d0a5629f789ac8ed4e0d79782760
Daniel Chen <s1300045@gmail.com>
2021-02-20 12:22:25 -0600

    Fixes gravity too strong.

--- src/plugins/platformer/constants.ts
+++ src/plugins/platformer/constants.ts
@@ -1,4 +1,4 @@
 export const RADIAN_45: number = 0.7853982;
 export const SQRT_2: number = 1.41421356237;
 export const SQRT_2_DIV_2: number = 1.41421356237 / 2;
-export const GRAVITY: number = 1.3;
+export const GRAVITY: number = 0.3;
